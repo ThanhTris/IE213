f0a2174f2a84fdd56061044a6be79de6ce1425d0
rosy179 <lerosy179@gmail.com>
2026-04-09 08:33:12 +0000

    update router create-new-product

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import CreateNewProduct from "./pages/admin/CreateNewProduct";
+
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/create-new-product" element={<CreateNewProduct />} />
       </Routes>
     </div>
   );
